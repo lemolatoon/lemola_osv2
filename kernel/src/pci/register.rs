7844bc99abe50459b198a9c7cf4842a54fc35109
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-06-11 03:25:28 +0900

    Fix calculation in PciConfigAddress::new

--- kernel/src/pci/register.rs
+++ kernel/src/pci/register.rs
@@ -1,6 +1,6 @@
 extern crate alloc;
 use alloc::vec::Vec;
-use core::arch::global_asm;
+use core::{arch::asm, fmt};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct PciConfigAddress(u32);
@@ -12,7 +12,7 @@ impl PciConfigAddress {
         address |= (bus as u32) << 16;
         address |= (device as u32) << 11;
         address |= (function as u32) << 8;
-        address |= (register & 0b1100) as u32;
+        address |= (register & 0b111_11100) as u32;
         Self(address)
     }
 
@@ -29,7 +29,7 @@ const CONFIG_DATA: u16 = 0xcfc;
 unsafe fn write_address(address: PciConfigAddress) {
     io_out_32(CONFIG_ADDRESS, address.0);
 }
-unsafe fn read_date() -> u32 {
+unsafe fn read_data_raw() -> u32 {
     io_in_32(CONFIG_DATA)
 }
 
@@ -66,23 +66,40 @@ impl PciDevice {
         self.class_code.base() == 0x06 && self.class_code.sub() == 0x04
     }
 
-    pub fn read_bar_32(&self, bar_index: u8) -> Option<u32> {
-        let bar = read_data(PciConfigAddress::new_from_bar_index(
-            self.bus,
-            self.device,
-            self.function,
-            bar_index,
-        )?);
-        return Some(bar);
+    pub fn read_configuration_space(&self, addr: u8) -> u32 {
+        let addr = PciConfigAddress::new(self.bus, self.device, self.function, addr);
+        unsafe {
+            write_address(addr);
+            read_data_raw()
+        }
     }
 
-    pub fn read_bar_64(&self, bar_index: u8) -> Option<u64> {
+    pub fn read_bar(&self, bar_index: u8) -> Option<u64> {
+        // Address and size of the BAR (https://wiki.osdev.org/PCI#Header_Type_0x0)
+        // When you want to retrieve the actual base address of a BAR,
+        // be sure to mask the lower bits.
+        // For 16-bit Memory Space BARs, you calculate (BAR[x] & 0xFFF0).
+        // For 32-bit Memory Space BARs, you calculate (BAR[x] & 0xFFFFFFF0).
+        // For 64-bit Memory Space BARs, you calculate ((BAR[x] & 0xFFFFFFF0) + ((BAR[x + 1] & 0xFFFFFFFF) << 32))
+        // For I/O Space BARs, you calculate (BAR[x] & 0xFFFFFFFC).
         let bar = read_data(PciConfigAddress::new_from_bar_index(
             self.bus,
             self.device,
             self.function,
             bar_index,
         )?);
+        // 0    : メモリ空間インジケーター 0
+        // 2..1 : タイプ 0  = 32bitメモリ空間, 2 = 64bitメモリ空間
+        // 3    : プレフェッチ許可 1 = プレフェッチ許可
+        // 31..4: ベースアドレス
+        log::info!("memory space indicator: {}", bar & 0b1);
+        log::info!("type: {}", (bar >> 1) & 0b11);
+        log::info!("prefetchable: {}", (bar >> 3) & 0b1);
+        log::info!("base address: {:x}", (bar >> 4) & 0xffff_ffff);
+        if (bar >> 1) & 0b11 == 0 {
+            // 32bit address
+            return Some(bar as u64);
+        }
         let bar_upper = read_data(PciConfigAddress::new_from_bar_index(
             self.bus,
             self.device,
@@ -90,6 +107,23 @@ impl PciDevice {
             bar_index + 1,
         )?);
         return Some(bar as u64 | ((bar_upper as u64) << 32));
+    }
+
+    /// read 32bit data from PCI config space
+    /// See also (https://wiki.osdev.org/PCI#Configuration_Space_Access_Mechanism_.231)
+    pub fn read_capabilities_pointer(&self) -> u8 {
+        debug_assert!(
+            self.header_type().is_generic_device() || self.header_type().is_pci_to_pci_bridge(),
+            "capabilities_pointer at 0x34 is only valid for generic device, but got {:?}",
+            self.header_type()
+        );
+        let raw_data = read_data(PciConfigAddress::new(
+            self.bus,
+            self.device,
+            self.function,
+            0x34,
+        ));
+        (raw_data & 0xff) as u8
     }
 
     pub const fn vendor_id(&self) -> VendorId {
@@ -135,6 +169,16 @@ pub struct ClassCode {
     interface: u8,
 }
 
+impl fmt::Display for ClassCode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{:#04x}{:02x}{:02x}",
+            self.base, self.sub, self.interface
+        )
+    }
+}
+
 impl ClassCode {
     pub fn new(bus: u8, device: u8, function: u8) -> Self {
         let raw_data = read_data(PciConfigAddress::new(bus, device, function, 0x08));
@@ -164,13 +208,19 @@ impl ClassCode {
         self.base == base && self.sub == sub && self.interface == interface
     }
 
-    pub const fn is_xhci(&self) -> bool {
+    pub const fn is_xhci_controller(&self) -> bool {
         self.matches(0x0c, 0x03, 0x30)
     }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct HeaderType(u8);
+
+impl fmt::Display for HeaderType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:#04x}", self.0)
+    }
+}
 
 impl HeaderType {
     pub fn new(bus: u8, device: u8, function: u8) -> Self {
@@ -181,6 +231,14 @@ impl HeaderType {
     /// from register offset 0x0c
     fn from_raw(raw_data: u32) -> Self {
         Self(((raw_data >> 16) & 0xff) as u8)
+    }
+
+    pub fn is_generic_device(&self) -> bool {
+        self.0 == 0
+    }
+
+    pub fn is_pci_to_pci_bridge(&self) -> bool {
+        self.0 == 1
     }
 
     pub fn is_multi_function(&self) -> bool {
@@ -206,6 +264,12 @@ impl DeviceId {
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct VendorId(u16);
 
+impl fmt::Display for VendorId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:#06x}", self.0)
+    }
+}
+
 impl VendorId {
     pub fn new(bus: u8, device: u8, function: u8) -> Self {
         let raw_data = read_data(PciConfigAddress::new(bus, device, function, 0));
@@ -229,30 +293,21 @@ impl VendorId {
 pub fn read_data(address: PciConfigAddress) -> u32 {
     unsafe {
         write_address(address);
-        read_date()
+        read_data_raw()
     }
 }
 
-extern "sysv64" {
-    fn io_out_32(address: u16, data: u32);
-    fn io_in_32(address: u16) -> u32;
+unsafe fn io_out_32(address: u16, data: u32) {
+    asm!(
+        "out dx, eax", in("dx") address, in("eax") data
+    )
 }
 
-global_asm!(
-    ".global io_out_32",
-    "io_out_32:",
-    "  mov dx, di",   // dx = address
-    "  mov eax, esi", // eax = data
-    "  out dx, eax",
-    "  ret",
-);
-global_asm!(
-    ".global io_in_32",
-    "  io_in_32:",
-    "  mov dx, di", // dx = address
-    "  in eax, dx", // eax = data (return value)
-    "  ret",
-);
+unsafe fn io_in_32(address: u16) -> u32 {
+    let data: u32;
+    asm!("in eax, dx", out("eax") data, in("dx") address);
+    data
+}
 
 pub fn scan_all_bus() -> Vec<PciDevice> {
     let mut devices = Vec::new();
@@ -302,5 +357,7 @@ pub fn scan_function(bus: u8, device: u8, function: u8, devices: &mut Vec<PciDev
         let secondary_bus_number = BusNumber::new(bus, device, function).secondary_bus_number();
         scan_bus(secondary_bus_number, devices);
     }
-    devices.push(pci_device);
+    if pci_device.vendor_id().is_valid() {
+        devices.push(pci_device);
+    }
 }
